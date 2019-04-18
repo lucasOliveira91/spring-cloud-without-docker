c537efb139aac51bb2a74af465003f847e6420d4
Lucas <ldoliveira91@outlook.com>
2019-04-18 08:07:32 -0300

    adds currency-exchange and currency-conversion services

---
+++ currency-exchange-service/src/main/java/com/example/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,27 @@
+package com.example.microservices.currencyexchangeservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/currency-exchange")
+public class CurrencyExchangeController {
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private ExchangeValueRepository exchangeValueRepository;
+
+    @GetMapping("/from/{from}/to/{to}")
+    public ExchangeValue retriveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        int local = Integer.parseInt(environment.getProperty("local.server.port"));
+        ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
+        exchangeValue.setPort(local);
+        return exchangeValue;
+    }
+}
---
+++ currency-exchange-service/src/main/java/com/example/microservices/currencyexchangeservice/ExchangeValue.java
@@ -0,0 +1,27 @@
+package com.example.microservices.currencyexchangeservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class ExchangeValue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "currency_from")
+    private String from;
+
+    @Column(name = "currency_to")
+    private String to;
+    private BigDecimal conversionMultiple;
+    private int port;
+}
