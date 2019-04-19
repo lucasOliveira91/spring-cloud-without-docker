2f13c00b53e8c6bbc547c654d64f51f39149b680
Lucas <ldoliveira91@outlook.com>
2019-04-19 12:49:48 -0300

    adds sleuth

--- currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,5 +1,7 @@
 package com.example.microservices.currencyconversionservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,8 @@ import java.math.BigDecimal;
 @RequestMapping("/currency-converter")
 public class CurrencyConversionController {
 
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private CurrencyExchangeServiceProxy proxy;
 
@@ -20,6 +24,8 @@ public class CurrencyConversionController {
                                                   @PathVariable String to,
                                                   @PathVariable BigDecimal quantity) {
         CurrencyConversionBean respo = proxy.retriveExchangeValue(from, to);
+        logger.info("requisição -> {}", respo);
+
         return new CurrencyConversionBean(respo.getId(), from, to, respo.getConversionMultiple(), respo.getQuantity(), quantity.multiply(respo.getConversionMultiple()), respo.getPort());
     }
 }
